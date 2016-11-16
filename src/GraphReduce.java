182fe8a57ba88bf5a4b597feaf98131deeb10771
Robert Lorentz <robey112@live.se>
2016-11-16 17:52:30 +0100

    Graphreader

--- src/GraphReduce.java
+++ src/GraphReduce.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 /**
  * Created by RobertLorentz on 15/11/16.
  */
 public class GraphReduce {
 
-Kattio io;
+    Kattio io;
+    private int m;
+
 
     GraphReduce() {
         io = new Kattio(System.in, System.out);
@@ -14,13 +18,24 @@ Kattio io;
     }
 
     public void translateToCasting(Graph graph) {
-
+        ArrayList<Graph.Vertex> vertexes = graph.getVertexes();
+        ArrayList<String> scenes = new ArrayList<>();
+        for(Graph.Vertex v : vertexes){
+            ArrayList<Graph.Edge> edges = v.getEdges();
+            if(edges.isEmpty()){
+                scenes.add("2 ".concat(Integer.toString(v.getValue())+" 1"));
+            }
+            for(Graph.Edge edge : edges) {
+                String s = "2 ".concat(Integer.toString(edge.getFrom())+" "+Integer.toString(edge.getTo()));
+                scenes.add(s);
+            }
+        }
     }
 
     public Graph readGraph() {
         int v = io.getInt();
         int e = io.getInt();
-        int m = io.getInt();
+        m = io.getInt();
         Graph graph = new Graph(v);
         for (int i = 0; i < e; i++) {
             int a = io.getInt();
